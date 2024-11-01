1a9602488c0771435ddbe4779335e19141a5e7c1
Shajin KP <shajin@screl.net>
2024-11-01 15:45:57 +0530

    S

--- test/esc_pos_bluetooth_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:esc_pos_bluetooth/esc_pos_bluetooth.dart';
-
-void main() {
-  test('Tests not implemented', () {
-    expect(1, 1);
-  });
-}
